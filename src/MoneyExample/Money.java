f9f3fa6992fb806c64ae0e681e9e553a5e28a87e
Jordan Wu <jordan.spencer.wu@gmail.com>
2016-12-01 23:09:14 -0700

    chapter 14 mixed currencies

--- src/MoneyExample/Money.java
+++ src/MoneyExample/Money.java
@@ -17,7 +17,7 @@ class Money implements Expression {
 		return new Money(amount, "CHF");
 	}
 	
-	Money times(int multiplier){
+	Expression times(int multiplier){
 		return new Money(amount * multiplier, currency);
 	};
 	
@@ -25,7 +25,7 @@ class Money implements Expression {
 		return currency;
 	}
 	
-	Expression plus(Money addend) {
+	public Expression plus(Expression addend) {
 		return new Sum(this, addend);
 	}
 	
